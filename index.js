4273a230b6069d149344b3ce8f3be89bf134878f
Mark Striemer <mstriemer@mozilla.com>
2017-03-30 12:30:43 -0700

    Open bugzilla bug

---
+++ index.js
@@ -0,0 +1,23 @@
+browser.contextMenus.create({
+  id: "open-bug",
+  title: "Open bugzilla bug",
+  contexts: ["selection"],
+});
+
+function openBug(info, tab) {
+  const id = /(\d+)/.exec(info.selectionText)[1];
+  if (id.length > 0) {
+    browser.tabs.create({
+      active: true,
+      url: `http://bugzil.la/${id}`,
+    });
+  }
+}
+
+browser.contextMenus.onClicked.addListener((info, tab) => {
+  switch (info.menuItemId) {
+    case "open-bug":
+      openBug(info, tab);
+      break;
+  }
+});
